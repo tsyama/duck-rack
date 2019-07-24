46325aa4c611db41692aa793ba7bcc7553037f12
tsyama <tsyama.desu@gmail.com>
2019-07-25 01:40:16 +0900

    TOPページの作成

---
+++ web-data/resources/views/Home/index.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>duck-rack</title>
+
+    <link href="/css/app.css" rel="stylesheet">
+
+</head>
+<body class="text-center">
+<div class="bg">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <h3 class="logo"><a href="/">duck-rack</a></h3>
+            <nav class="nav menu">
+                <a class="nav-link active" href="#">答える</a>
+                <a class="nav-link" href="#">見る</a>
+                <a class="nav-link" href="#">設定</a>
+            </nav>
+        </header>
+
+        <main role="main">
+            <p class="lead">自分語りはアヒルにやらせよう</p>
+            <h1 class="welcome-title">duck-rack</h1>
+            <p>
+                <a href="/login" class="btn btn-lg btn-success">
+                    <i class="fa fa-twitter"></i> ログイン
+                </a>
+            </p>
+        </main>
+
+        <footer class="mt-auto">
+            <p>&copy; 2019 <a href="https://twitter.com/tsyama_desu" target="_blank">@tsyama_desu</a></p>
+        </footer>
+    </div>
+</div>
+</body>
+</html>
