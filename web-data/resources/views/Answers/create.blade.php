4e3e81f504b1cbf289917c506a30cbd4db486eab
tsyama <tsyama.desu@gmail.com>
2019-08-18 13:44:31 +0900

    すべての質問に回答した場合の処理を追加

--- web-data/resources/views/Answers/create.blade.php
+++ web-data/resources/views/Answers/create.blade.php
@@ -8,31 +8,39 @@
             <img class="duck-icon" src="/img/duck-icon.jpg">
         </div>
     </div>
-    <div class="row">
-        <div class="col-12">
-            <p class="lead">
-                {{ $question->body }}
-            </p>
+    @if($question)
+        <div class="row">
+            <div class="col-12">
+                <p class="lead">
+                    {{ $question->body }}
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <p>
-                <textarea name="body" class="form-control" rows="5"></textarea>
-            </p>
+        <div class="row">
+            <div class="col-12">
+                <p>
+                    <textarea name="body" class="form-control" rows="5"></textarea>
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-6">
-            <a href="/answers/create" class="btn btn-lg btn-danger pull-left answer-btn">
-                他の質問
-            </a>
+        <div class="row">
+            <div class="col-6">
+                <a href="/answers/create" class="btn btn-lg btn-danger pull-left answer-btn">
+                    他の質問
+                </a>
+            </div>
+            <div class="col-6">
+                <button type="submit" class="btn btn-lg btn-success pull-right answer-btn">答える</button>
+            </div>
         </div>
-        <div class="col-6">
-            <button type="submit" class="btn btn-lg btn-success pull-right answer-btn">答える</button>
+        <input type="hidden" name="question_id" value="{{ $question->id }}">
+    @else
+        <div class="row">
+            <div class="col-12">
+                <p class="lead">すべての質問に回答済みです😮</p>
+            </div>
         </div>
-    </div>
-    <input type="hidden" name="question_id" value="{{ $question->id }}">
+    @endif
     {{ Form::close() }}
     <div class="row answer-row" style="margin-top: 30px">
         <div class="col-2 d-flex align-items-center">
